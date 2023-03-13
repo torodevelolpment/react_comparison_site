9b16201aa6b68ac5ecd18e95384975dfbf43f3fc
torodevelolpment <isaacrafaeltoroguzman@gmail.com>
2023-03-13 19:45:23 +0100

    Navbar 1.0

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,55 @@
-import React from 'react';
-import './navbar.css';
+import React, { useState } from 'react';
 //react icons
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../Assets/logo.png'
+//navbar menu import function
+import './navbar.css';
+const Menu = () => (
+  <>
+  <p><a href="#home">Home</a></p>
+  <p><a href="#whatgpt">What is GPT3</a></p>
+  <p><a href="probability">Open AI</a></p>
+  <p><a href="features">Case Studies</a></p>
+  <p><a href="#blog">Library</a></p>
+  </>
+)
+
 
 //We're writing the css classnames in a specific naming condition named BEM
 //BEM -> Block Element Modifier
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState (false)
   return (
     <div className="gpt__navbar">
       <div className="gpt__navbar-links">
-        <div className="gpt_navbar-links_logo">
+        <div className="gpt__navbar-links_logo">
           <img src={logo} alt="" />
         </div>
+        <div className="gpt__navbar-links_container">
+          <Menu />
+        </div>
+      </div>
+      <div className="gpt__navbar-sign">
+          <p>Sign in</p>
+          <button type="button">Sign up</button>
+      </div>
+      <div className="gpt__navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine color="#FFF" size={27} onClick={()=> setToggleMenu(false)} />
+          : <RiMenu3Line color="#FFF" size={27} onClick={()=> setToggleMenu(true)} />
+        
+        }
+        {toggleMenu &&(
+          <div className="gpt__navbar-menu_container scale-up-center">
+            <div className="gpt__navbar-menu_container-links">
+              <Menu />
+              <div className="gpt__navbar-menu_container-links-sign">
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
