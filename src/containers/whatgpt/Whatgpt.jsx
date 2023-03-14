9f138b5cec627c61a756c253e76450726dc001a2
torodevelolpment <isaacrafaeltoroguzman@gmail.com>
2023-03-14 23:21:03 +0100

    Brand & features 1.0 - whatgpt 50%

--- src/containers/whatgpt/Whatgpt.jsx
+++ src/containers/whatgpt/Whatgpt.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import './whatgpt.css';
+import { Feature } from '../../components';
 
 const Whatgpt = () => {
   return (
-    <div>Whatgpt</div>
+    <div className="gpt__whatgpt section__margin" id='whatgpt'>
+      <div className="gpt__whatgpt-feature">
+        <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
+      </div>
+      <div className="gpt__whatgpt-heading">
+        <h1 className="gradient__text">
+          The Possibilities are beyond your imagination
+        </h1>
+        <p>Explore the Library</p>
+      </div>
+      <div className="gpt__whatgpt-container">
+        <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. "/>
+        <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments"/>
+        <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments"/>
+      </div>
+    </div>
   )
 }
 
