be4bcbcf349ab16dff9777844f35b366f9c238a5
torodevelolpment <isaacrafaeltoroguzman@gmail.com>
2023-05-10 22:38:12 +0200

    education section added

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import './features.css';
+import { Feature } from '../../components';
 
-const Feature = () => {
+
+const featuresData = [
+  {
+    title: 'International Studies',
+    text: 'Universidad Santa María. Caracas, Venezuela.'
+  },
+  {
+    title: 'Master en Dirección de Marketing, Branding, Entorno Digital y Sostenibilidad',
+    text: 'Universitat de Barcelona. Barcelona, España.'
+  },
+] 
+
+const Features = () => {
   return (
-    <div>Feature</div>
+    <div className="mycv__features section__padding" id="suagatam">
+      <div className="mycv__features-heading">
+        <h1 className="gradient__text">
+          Here's my Academic History</h1>
+        <p>Download my CV here!</p>
+      </div>
+      <div className="mycv__features-container">
+        {featuresData.map((item, index) => (
+          <Feature title={item.title} text={item.text} key={item.title + index}/>
+        ) 
+        )}
+      </div>
+    </div>
   )
 }
 
-export default Feature
+export default Features
--- src/containers/whatgpt/Whatgpt.jsx
+++ src/containers/whatgpt/Whatgpt.jsx
@@ -7,16 +7,16 @@ const Whatgpt = () => {
   return (
     <div className="gpt__whatgpt section__margin" id="whatgpt">
       <div className="gpt__whatgpt-feature">
-        <Featuretitle title="Who am i as a proffesional?" text="As a Performance Marketing Professional, I have developed a range of skills and proficiencies that enable me to manage digital marketing campaigns effectively. I am experienced in campaign management across channels such as search, social media, display advertising, and email marketing, and I am skilled in using analytics tools to track performance and make data-driven decisions. My basic knowledge of new technologies and programming languages like HTML, CSS, and JS allows me to understand and get involved better into the IT sector and create the right sinergy between marketing and technology. I have worked in a variety of industries, and I am always eager to learn new skills to stay on top of industry trends." />
+        <Featuretitle title="Who am i as a professional?" text="As a Performance Marketing Professional, I have developed a range of skills and proficiencies that enable me to manage digital marketing campaigns effectively. I am experienced in campaign management across channels such as search, social media, display advertising, and email marketing, and I am skilled in using analytics tools to track performance and make data-driven decisions. My basic knowledge of new technologies and programming languages like HTML, CSS, and JS allows me to understand and get involved better into the IT sector and create the right sinergy between marketing and technology. I have worked in a variety of industries, and I am always eager to learn new skills to stay on top of industry trends." />
         <div className="gpt__whatgpt-feature-img">
-            <img src={logotext} alt="photo"/>
+            <img src={logotext} alt="photopapita"/>
         </div>
       </div>
       <div className="gpt__whatgpt-heading">
         <h1 className="gradient__text">
-          The Possibilities are beyond your imagination
+          "Luck is when opportunity meets preparation"
         </h1>
-        <p>Explore the Library</p>
+        <p>Download my CV</p>
       </div>
       <div className="gpt__whatgpt-container">
         <Feature title="Education" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. "/>
