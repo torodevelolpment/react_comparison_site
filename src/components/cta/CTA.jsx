65754ea29244786d81bcadcd7fd396a589b9b7e4
torodevelolpment <isaacrafaeltoroguzman@gmail.com>
2023-05-11 13:12:49 +0200

    secciones: job experience and footer added

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,14 @@ import './cta.css';
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className="mycv__cta">
+      <div className="mycv__cta-content">
+        <h3>You can download my CV here</h3>
+      </div>
+      <div className="mycv__cta-btn">
+        <button type="button">Download PDF</button>
+      </div>
+    </div>
   )
 }
 
