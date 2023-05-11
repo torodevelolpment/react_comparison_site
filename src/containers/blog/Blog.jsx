96aaaff17bdcd1dbea14f07ac0dc44e3b90c6aac
torodevelolpment <isaacrafaeltoroguzman@gmail.com>
2023-05-11 17:32:45 +0200

    Licenses and certifications done

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -5,19 +5,19 @@ import {blog01, blog02, blog03, blog04, blog05} from './imports';
 
 const Blog = () => {
   return (
-    <div className="mycv__blog section__padding">
+    <div className="mycv__blog section__padding" id="licencias">
       <div className="mycv__blog-heading">
         <h1 className="gradient__text">licences and Certifications</h1>
       </div>
       <div className="mycv__blog-container">
         <div className="mycv__blog-container_groupA">
-          <Article imageUrl={blog01} />                  
+          <Article imgUrl={blog01} date="May 11 2023" title="put your licence or certification here" />                  
         </div>
         <div className="mycv__blog-container_groupB">
-          <Article imageUrl={blog02} />          
-          <Article imageUrl={blog03} />          
-          <Article imageUrl={blog04} />          
-          <Article imageUrl={blog05} />          
+          <Article imgUrl={blog02} date="May 11 2023" title="put your licence or certification here" />          
+          <Article imgUrl={blog03} date="May 11 2023" title="put your licence or certification here" />          
+          <Article imgUrl={blog04} date="May 11 2023" title="put your licence or certification here" />          
+          <Article imgUrl={blog05} date="May 11 2023" title="put your licence or certification here" />          
         </div>
       </div>
     </div>
