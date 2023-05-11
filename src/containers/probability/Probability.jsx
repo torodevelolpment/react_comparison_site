268b39116270705d8990a36cc1f05a20589ec8b9
torodevelolpment <isaacrafaeltoroguzman@gmail.com>
2023-05-11 17:57:12 +0200

    rrss links added

--- src/containers/probability/Probability.jsx
+++ src/containers/probability/Probability.jsx
@@ -4,17 +4,21 @@ import experienceImage from '../../Assets/logo.png'
 
 const Probability = () => {
   return (
-    <div className="mycv__experience section__padding" id="#experience">
-      <div className="mycv__experience-img">
-        <img src={experienceImage} alt="experience"/>
-      </div>
-      <div className="mycv__experience-content">
-        <h4>Request early access to get started</h4>
-        <h1 className="gradient__text">My Job Experience</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt modi perferendis voluptas, exercitationem assumenda perspiciatis dolore nulla eveniet harum maiores?</p>
-        <h4>pepetopepeton</h4>
-      </div>
+    <><div className="mycv__experience section__padding" id="#experience">
+        <div className="mycv__experience-img">
+          <img src={experienceImage} alt="experience" />
+        </div>
+        <div className="mycv__experience-content">
+          <h4>Request early access to get started</h4>
+          <h1 className="gradient__text">My Job Experience</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt modi perferendis voluptas, exercitationem assumenda perspiciatis dolore nulla eveniet harum maiores?</p>
+          <h4>pepetopepeton</h4>
+        </div>
     </div>
+    <div>
+
+    </div>
+    </>
   )
 }
 
