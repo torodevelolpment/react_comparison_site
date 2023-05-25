f34048be5a0f60c57d408d7fa52aa559abd385f6
torodevelolpment <isaacrafaeltoroguzman@gmail.com>
2023-05-25 14:19:57 +0200

    section margins fixed

--- src/containers/whatgpt/Whatgpt.jsx
+++ src/containers/whatgpt/Whatgpt.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './whatgpt.css';
-import { Heroproduct, Feature, Featuretitle, Carousel } from '../../components';
+import { Carousel, Feature } from '../../components';
 import logotext from '../../Assets/logotext.png';
 
+const featuresDataSection = [
+  {
+    title: 'From the ground to your table',
+  },
+] 
 
 const Whatgpt = () => {
   return (
@@ -11,10 +16,11 @@ const Whatgpt = () => {
         {/* <Heroproduct/> */}
         <Carousel/>
       </div>
-      <div className="gpt__whatgpt-heading flex flex-col items-center justify-center justify-items-center text-center section__padding">
-        <h1 className="gradient__text">
-          Our Sustainability Values
-        </h1>
+      <div className="gpt__whatgpt-heading flex flex-col section__padding">
+         {featuresDataSection.map((item, index) => (
+          <Feature title={item.title} key={item.title + index}/>
+        ) 
+        )}
         <p className="text-black mt-2.5">At Valle Encantado Produce we believe that the quality and sustainability of our products depend on our direct commitment to our farmers. Through innovative and modern techniques that have been duly tested, we train our partners in field management and traceability in order to increase productivity and achieve high-quality products.</p>
       </div>
     </div>
